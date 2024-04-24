c30c21c5535b81d58b809f343717c55abb88b696
Matt <matt@mousybusiness.com>
2024-04-24 18:21:42 +0100

    chore: setup stubbed routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,12 +2,41 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"net/http"
 )
 
 func main() {
-	for {
-		fmt.Println("Running", time.Now())
-		time.Sleep(time.Second)
+	mux := http.NewServeMux()
+
+	// Users should be able to fetch a cake by ID.
+	mux.HandleFunc("GET /v1/cake/{id}/", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		fmt.Fprintf(w, "got cake with id: %v\n", id)
+	})
+
+	// Users should be able to list all cakes.
+	mux.HandleFunc("GET /v1/cakes/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "list cakes\n")
+	})
+
+	// Users should be able to search for a cake by yumFactor and/or name.
+	mux.HandleFunc("POST /v1/cakes/search/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "search cakes\n")
+	})
+
+	// Users should be able to add another cake.
+	mux.HandleFunc("PUT /v1/cake/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "create cake\n")
+	})
+
+	// Users should be able to delete an existing cake.
+	mux.HandleFunc("DELETE /v1/cake/{id}/", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		fmt.Fprintf(w, "delete cake: %v\n", id)
+	})
+
+	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
+		log.Panic(err)
 	}
 }
